303f4606772757c1775b9cc7b9c67448f390b54a
raywu <rwu13795@gmail.com>
2022-04-16 03:26:00 -0400

    configuring socket-io cors

--- src/socket-io/socketConnection.ts
+++ src/socket-io/socketConnection.ts
@@ -1,11 +1,7 @@
 import { io } from "socket.io-client";
 
 export default function connectSocket(user_id: string, username: string) {
-  const server = process.env.REACT_APP_SERVER_URL
-    ? process.env.REACT_APP_SERVER_URL
-    : "http://localhost:5000";
-
-  const socket = io(server, {
+  const socket = io(process.env.REACT_APP_SERVER_URL!, {
     // use the "handshake" to let server to identify the current user
     query: { user_id, username },
     // withCredentials: true,

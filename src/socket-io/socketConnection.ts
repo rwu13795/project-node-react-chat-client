9916dadcfe0fa722282cc001275d17ef614b1900
raywu <rwu13795@gmail.com>
2022-04-19 16:20:50 -0400

    adding signed-cookies

--- src/socket-io/socketConnection.ts
+++ src/socket-io/socketConnection.ts
@@ -4,7 +4,6 @@ export default function connectSocket(user_id: string, username: string) {
   const socket = io(process.env.REACT_APP_SERVER_URL!, {
     // use the "handshake" to let server to identify the current user
     query: { user_id, username },
-    // withCredentials: true,
   });
 
   return socket;
